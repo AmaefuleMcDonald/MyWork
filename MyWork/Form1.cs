661e72b398fba40f7e32975d539739063b77cb3b
AmaefuleMcDonald <amaefule720@gmail.com>
2023-10-17 14:59:10 +0100

    Add project files.

---
+++ MyWork/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            if(pictureBox1.Image == null)
+            {
+                pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            }
+
+            var graphics = Graphics.FromImage(pictureBox1.Image);
+
+            graphics.Clear(Color.Transparent);
+
+            if ( "Circle".Equals(textBox1.Text))
+            {
+                graphics.FillEllipse(Brushes.Aquamarine, 10, 10, 100, 100);
+            }
+
+            if ("Rectangle".Equals(textBox1.Text))
+            {
+                graphics.FillRectangle(Brushes.Aquamarine, 10, 10, 100, 100);
+            }
+            // MessageBox.Show("Hello");
+            pictureBox1.Refresh();
+        }
+        
+    }
+}
